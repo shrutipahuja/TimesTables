8a557e777858ca25c0f60513394ab21c8aa58233
shruti.pahuja <shruti.pahuja@accenture.com>
2020-02-05 13:58:42 +0530

    Removed magic numbers

--- app/src/main/java/com/example/timestables/MainActivity.java
+++ app/src/main/java/com/example/timestables/MainActivity.java
@@ -24,9 +24,11 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         SeekBar seekBar = findViewById(R.id.seekBar);
 
-        seekBar.setMax(20);
-        seekBar.setMin(1);
-        seekBar.setProgress(1);
+        int maxSeekValue = 20;
+        int minSeekValue = 1;
+        seekBar.setMax(maxSeekValue);
+        seekBar.setMin(minSeekValue);
+        seekBar.setProgress(minSeekValue);
         multiples = new ArrayList<>(asList(1,2,3,4,5,6,7,8,9,10));
 
         final ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, multiples);
@@ -36,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 multiples.clear();
-                for(int j =1; j<=10; j++) {
+                int maxMultiple = 10;
+                for(int j =1; j <= maxMultiple; j++) {
                     multiples.add(i*j);
                     arrayAdapter.notifyDataSetChanged();
                 }
